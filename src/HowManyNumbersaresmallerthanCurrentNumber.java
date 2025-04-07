8a1299162947290f8aff6a1b42d9735a73cdebb6
adshreyash <shreyash.tiwari.st08@gmail.com>
2025-04-07 22:26:03 +0530

    QUESTION of Kunal Kushwaha leetcode

--- src/HowManyNumbersaresmallerthanCurrentNumber.java
+++ src/HowManyNumbersaresmallerthanCurrentNumber.java
@@ -9,11 +9,11 @@ public class HowManyNumbersaresmallerthanCurrentNumber {
         int[] ans = new int[nums.length];
         for(int i = 0; i < nums.length; i++) {
             int c = 0;
-            for(int j = 0; j < nums.length; j++) {
-                if(nums[i]==nums[j]){
+            for (int num : nums) {
+                if (nums[i] == num) {
                     continue;
                 }
-                if(nums[i]>nums[j]){
+                if (nums[i] > num) {
                     c++;
                 }
             }
--- src/NumberofGoodPairs_1512.java
+++ src/NumberofGoodPairs_1512.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 
 public class NumberofGoodPairs_1512 {
     public static void main(String[] args) {
